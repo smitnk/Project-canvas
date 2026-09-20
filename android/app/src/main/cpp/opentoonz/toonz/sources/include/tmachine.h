70dfd7352e51b0c6a39d53537b6dbc2cad7bcd72
smitnk <sumitnaikna29@gmail.com>
2026-09-21 01:02:40 +0530

    Fix Android OpenToonz platform type and GLUT guards

--- android/app/src/main/cpp/opentoonz/toonz/sources/include/tmachine.h
+++ android/app/src/main/cpp/opentoonz/toonz/sources/include/tmachine.h
@@ -7,7 +7,7 @@
 #define TNZ_MACHINE_CHANNEL_ORDER_BGRM 1
 #elif defined(__sgi)
 #define TNZ_MACHINE_CHANNEL_ORDER_MBGR 1
-#elif defined(LINUX) || defined(FREEBSD) || defined(HAIKU)
+#elif defined(LINUX) || defined(FREEBSD) || defined(HAIKU) || defined(__ANDROID__)
 #define TNZ_MACHINE_CHANNEL_ORDER_BGRM 1
 #elif defined(MACOSX)
 #define TNZ_MACHINE_CHANNEL_ORDER_MRGB 1
