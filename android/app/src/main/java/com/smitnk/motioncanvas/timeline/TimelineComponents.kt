62b1f8ace19d3b5cd69d940b15f7e12dacdbc985
smitnk <sumitnaikna29@gmail.com>
2026-09-20 18:48:41 +0530

    Fix timeline import syntax

--- android/app/src/main/java/com/smitnk/motioncanvas/timeline/TimelineComponents.kt
+++ android/app/src/main/java/com/smitnk/motioncanvas/timeline/TimelineComponents.kt
@@ -6,7 +6,8 @@ import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickableimport androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
