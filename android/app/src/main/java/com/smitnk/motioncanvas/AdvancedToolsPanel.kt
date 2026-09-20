5e741867b5cf65f7ad910c1e0340ab60bf094813
smitnk <sumitnaikna29@gmail.com>
2026-09-20 21:53:44 +0530

    Import particle engine types used by advanced tools

--- android/app/src/main/java/com/smitnk/motioncanvas/AdvancedToolsPanel.kt
+++ android/app/src/main/java/com/smitnk/motioncanvas/AdvancedToolsPanel.kt
@@ -6,6 +6,8 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.smitnk.motioncanvas.animation.*
+import com.smitnk.motioncanvas.tools.ParticleEngine
+import com.smitnk.motioncanvas.tools.ParticleEmitter
 import com.smitnk.motioncanvas.camera.Camera2D
 import com.smitnk.motioncanvas.selection.MultiFrameTransform
 
