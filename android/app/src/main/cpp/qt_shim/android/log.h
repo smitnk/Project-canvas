d4c918f7adbd03f355e9d4458fcaf1edcfa5b83a
smitnk <sumitnaikna29@gmail.com>
2026-09-20 21:03:09 +0530

    Fix Android C logging header for C compiler

--- android/app/src/main/cpp/qt_shim/android/log.h
+++ android/app/src/main/cpp/qt_shim/android/log.h
@@ -1,6 +1,6 @@
 #ifndef ANDROID_LOG_H
 #define ANDROID_LOG_H
-#include <cstdio>
+#include <stdio.h>
 #define ANDROID_LOG_INFO 4
 #define ANDROID_LOG_ERROR 6
 #define __android_log_print(prio, tag, ...) printf("[" tag "] " __VA_ARGS__)
