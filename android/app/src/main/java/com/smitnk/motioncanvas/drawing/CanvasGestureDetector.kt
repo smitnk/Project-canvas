a7dee0c0a27faa1edbada5c6177fd476cff0715a
smitnk <sumitnaikna29@gmail.com>
2026-09-20 21:53:54 +0530

    Import Compose awaitFirstDown used by gesture detector

--- android/app/src/main/java/com/smitnk/motioncanvas/drawing/CanvasGestureDetector.kt
+++ android/app/src/main/java/com/smitnk/motioncanvas/drawing/CanvasGestureDetector.kt
@@ -1,6 +1,7 @@
 package com.smitnk.motioncanvas.drawing
 
 import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.awaitFirstDown
 import androidx.compose.foundation.gestures.forEachGesture
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
