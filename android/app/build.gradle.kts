b2dee14c4337df8a02f561d570d0ba97ae14eefb
smitnk <sumitnaikna29@gmail.com>
2026-09-22 13:19:23 +0530

    pin Android NDK 27 for 16KB native support

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
 android {
     namespace = "com.smitnk.motioncanvas"
     compileSdk = 36
+    ndkVersion = "27.0.12077973"
 
     defaultConfig {
         applicationId = "com.smitnk.motioncanvas"
