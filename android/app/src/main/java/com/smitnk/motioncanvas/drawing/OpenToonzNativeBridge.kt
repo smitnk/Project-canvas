f7487081c940dd5c2a361ea46133fd8e2ab2976f
smitnk <sumitnaikna29@gmail.com>
2026-09-22 13:18:46 +0530

    log OpenToonz native load failures

--- android/app/src/main/java/com/smitnk/motioncanvas/drawing/OpenToonzNativeBridge.kt
+++ android/app/src/main/java/com/smitnk/motioncanvas/drawing/OpenToonzNativeBridge.kt
@@ -1,5 +1,7 @@
 package com.smitnk.motioncanvas.drawing
 
+import android.util.Log
+
 /**
  * JNI Bridge to OpenToonz Native Drawing Engine C++ library (libopentoonz_drawing_engine.so).
  * Upstream OpenToonz commit: 065cc1404ba43019b22a2577d529134c3e01931b (v1.8.0)
@@ -7,18 +9,28 @@ package com.smitnk.motioncanvas.drawing
  */
 object OpenToonzNativeBridge {
 
+    private const val TAG = "OpenToonzNativeBridge"
     private var isNativeLoaded = false
+    private var loadError: Throwable? = null
 
     init {
         try {
             System.loadLibrary("opentoonz_drawing_engine")
             isNativeLoaded = nativeInitEngine()
         } catch (t: Throwable) {
+            loadError = t
             isNativeLoaded = false
+            Log.e(
+                TAG,
+                "OpenToonz native engine failed to load; software drawing fallback will be used",
+                t
+            )
         }
     }
 
     fun isAvailable(): Boolean = isNativeLoaded
+
+    fun getLoadError(): Throwable? = loadError
 
     external fun nativeInitEngine(): Boolean
     external fun nativeGetCommitSha(): String
